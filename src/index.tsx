b54b0c8ea8902e8a0fda454ef5561f99576ede75
Dmitriy Pechenyuk <dmitriypechenyuk0@gmail.com>
2025-11-19 13:22:18 +0200

    create repo

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import { App } from "./App"
 
 const rootContainer = document.getElementById('root') as HTMLElement
 
-
-// 2. Создаем "корень" React-приложения.
-// С помощью createRoot() мы говорим React, что rootContainer
-// будет контейнером для нашего приложения.
 const root = createRoot(rootContainer)
 
 root.render(<App></App>)
