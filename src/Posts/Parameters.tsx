79216c0eeaff6a6eeef598ecfb66a41e32d15660
Dmitriy Pechenyuk <dmitriypechenyuk0@gmail.com>
2025-11-30 08:50:28 +0200

    complete likes div

--- src/Posts/Parameters.tsx
+++ src/Posts/Parameters.tsx
@@ -1,8 +1,10 @@
 import pstyles from './css/postParameters.module.css'
 import LoupeIcon from '../assets/svg/loupe.svg';
 import TagIcon from '../assets/svg/Tag.svg';
+import LikeIcon from '../assets/svg/Like.svg'
 import { Tags } from './PostList';
 import unfilledCheckbox from '../assets/svg/unfilledcheckbox.svg'
+import radioButtonIcon from '../assets/svg/radio.svg'
 
 export function DisplayTags(props: {id: number, title: string}){
     const {title, id} = props
@@ -21,7 +23,7 @@ export function Parameters(){
             </div>
             <div className={pstyles.Params} >
                 <p>Search by parameters</p>
-                <div>
+                <div className={pstyles.ParamsTagsDiv}  >
                     <div className={pstyles.tagsTopBar}>
                         <div>
                             <img src={TagIcon} alt="TagIcon" />
@@ -39,6 +41,18 @@ export function Parameters(){
                             </div>
                         })}
                         
+                    </div>
+                </div>
+                <div className={pstyles.likesDiv} >
+                    <div className={pstyles.likesDivTopBar}>
+                        <img src={LikeIcon} />
+                        <p>Likes</p>
+                    </div>
+                    <div className={pstyles.likesDivParams}>
+                        <div className={pstyles.likeDiv}><img src={radioButtonIcon}/><p>Less than 0</p></div>
+                        <div className={pstyles.likeDiv}><img src={radioButtonIcon}/><p>Greater than 0</p></div>
+                        <div className={pstyles.likeDiv}><img src={radioButtonIcon}/><p>Greater than 50</p></div>
+                        <div className={pstyles.likeDiv}><img src={radioButtonIcon}/><p>Greater than 100</p></div>
                     </div>
                 </div>
             </div>
